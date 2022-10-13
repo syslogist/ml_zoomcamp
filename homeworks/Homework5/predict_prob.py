94b7eea6deab1568f15181453fd44a0d17e70a01
Amiya Bhattacharya <76175447+syslogist@users.noreply.github.com>
2022-10-13 11:19:42 -0400

    Add files via upload

---
+++ homeworks/Homework5/predict_prob.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pickle
+from flask import Flask, request, jsonify
+
+model_file = 'model1.bin'
+dv_file = 'dv.bin'
+
+with open(model_file,'rb') as f_model:
+    model = pickle.load(f_model)
+
+with open(dv_file,'rb') as f_dv:
+    dv = pickle.load(f_dv)
+
+app = Flask('credit_card')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    client = request.get_json()
+    X = dv.transform([client])
+    y_prob_pred = model.predict_proba(X)[0,1]
+    card = (y_prob_pred >= 0.5)
+    result = {'card_probability': float(y_prob_pred), 'card':bool(card)}
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
+
+
